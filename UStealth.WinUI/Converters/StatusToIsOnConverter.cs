5f0b87aab98f8ff75638585a36f5ad957490e9c3
James Alickolli <jalickolli@gmail.com>
2025-09-08 09:16:56 +1000

    started making winui app native aot

--- UStealth.WinUI/Converters/StatusToIsOnConverter.cs
+++ UStealth.WinUI/Converters/StatusToIsOnConverter.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace UStealth.WinUI.Converters
 {
-    public class StatusToIsOnConverter : IValueConverter
+    public partial class StatusToIsOnConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
