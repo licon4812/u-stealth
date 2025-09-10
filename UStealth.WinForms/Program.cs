eba33c19dd8c7f65dce8ff876abff9fa9a6c6e94
James Alickolli <jalickolli@gmail.com>
2025-09-10 16:40:27 +1000

    Winforms app supports darkmode

--- UStealth.WinForms/Program.cs
+++ UStealth.WinForms/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace UStealth
 {
@@ -14,7 +15,34 @@ namespace UStealth
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.SetDefaultFont(new Font(new FontFamily("Microsoft Sans Serif"), 8f));
+            if (IsDarkModeAvailable())
+            {
+                Application.SetColorMode(SystemColorMode.Dark);
+            }
             Application.Run(new ToggleMain());
+        }
+
+        /// <summary>
+        /// Checks if dark mode is available (system theme is dark).
+        /// </summary>
+        public static bool IsDarkModeAvailable()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize"))
+                {
+                    if (key != null)
+                    {
+                        object value = key.GetValue("AppsUseLightTheme");
+                        if (value is int intValue)
+                        {
+                            return intValue == 0; // 0 = dark mode, 1 = light mode
+                        }
+                    }
+                }
+            }
+            catch { }
+            return false;
         }
     }
 }
