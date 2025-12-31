058cdee2409df83d2a940952ebfcd893c04aa960
James Alickolli <32421608+licon4812@users.noreply.github.com>
2025-12-31 15:23:40 +1100

    [Legacy] Added System Theming  to WinForms app (#17)
    * made winforms app dotnet 10
    
    * Winforms app supports darkmode
    
    * Update Program.cs
    
    * Winforms app responds to theme changes
    
    * Fixed Header cells not using system theme Winforms
    
    * Update UStealth.WinForms.csproj

--- UStealth.WinForms/Program.cs
+++ UStealth.WinForms/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace UStealth
 {
@@ -14,7 +15,55 @@ namespace UStealth
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.SetDefaultFont(new Font(new FontFamily("Microsoft Sans Serif"), 8f));
+            Application.SetColorMode(SystemColorMode.System);
+
+            // Listen for system color/theme changes and update the app while running
+            SystemEvents.UserPreferenceChanged += OnUserPreferenceChanged;
+            Application.ApplicationExit += (_, __) => SystemEvents.UserPreferenceChanged -= OnUserPreferenceChanged;
+
             Application.Run(new ToggleMain());
+        }
+
+        private static void OnUserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            // Respond to relevant categories only
+            if (e.Category is not (UserPreferenceCategory.General or UserPreferenceCategory.Color
+                or UserPreferenceCategory.VisualStyle)) return;
+
+            void Apply()
+            {
+                // Re-apply System color mode and refresh open forms
+                Application.SetColorMode(SystemColorMode.System);
+                foreach (Form f in Application.OpenForms)
+                {
+                    try
+                    {
+                        f.Invalidate(true);
+                        f.Refresh();
+                        f.Update();
+                    }
+                    catch { /* ignore repaint errors */ }
+                }
+            }
+
+            // Marshal to UI thread if needed
+            if (Application.OpenForms.Count > 0)
+            {
+                var anyForm = Application.OpenForms[0];
+                if (anyForm.IsHandleCreated && anyForm.InvokeRequired)
+                {
+                    try { anyForm.BeginInvoke((Action)Apply); } catch { }
+                }
+                else
+                {
+                    Apply();
+                }
+            }
+            else
+            {
+                // No forms yet; still re-apply app-wide setting
+                Application.SetColorMode(SystemColorMode.System);
+            }
         }
     }
 }
